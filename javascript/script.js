6669bfb2bed581e20ef604f5e9dfa0988328620d
Bruce Perry <bruce@iperry.net>
2014-11-17 20:16:20 -0800

    Formatting Begins

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,31 @@
 window.onload = function () {
-  var color = prompt("What is your favorite color?", "Albaster");
-  if (color !== null) {
+  var name = prompt("WHAT'S YOUR NAME?", "[NAME]");
+  if (name !== null) {
+    console.log(name) ;
+    document.getElementById("name").innerHTML = name;
+  }
+}
+
+function getUserInput() {
+  var color = prompt("WHAT'S YOUR FAVORITE COLOR?", "[COLOR]");
+  if (color != null) ; {
     console.log(color) ;
     document.getElementById("color").innerHTML = color;
   }
 }
 
-function getUserInput() {
-  var name = prompt("What is your Name?", "Frank");
-  if (name != null) ; {
-    console.log(name) ;
-    document.getElementById("name").innerHTML = name;
-  }
-}
+//window.onload = function () {
+//  var color = prompt("What is your favorite color?", "[COLOR]");
+//  if (color !== null) {
+//    console.log(color) ;
+//    document.getElementById("color").innerHTML = color;
+//  }
+//}
+//
+//function getUserInput() {
+//  var name = prompt("What is your Name?", "[NAME]");
+//  if (name != null) ; {
+//    console.log(name) ;
+//    document.getElementById("name").innerHTML = name;
+//  }
+//}
